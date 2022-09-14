20ea854f9655ee4b8edd956702ffa00e5123426f
NickBwalley <NickBwalley@gmail.com>
2022-09-14 16:44:05 +0300

    react refresher

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
+const BookList = () => {
   return (
-    <div>
-      <h4>Hello World</h4>
-      <input type="text" name="" id="" />
-    </div>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 };
 
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
+    alt=""
+  ></img>
+);
+
+const Title = () => <h1>I love you to the moon and back</h1>;
+const Author = () => <h4>Amelia Hepworth</h4>;
 // const App2 = () => {
 //   return React.createElement("h1", {}, React.createElement('h1', {}, "hello world");
 // };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BookList />, document.getElementById('root'));
