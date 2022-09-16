2c6525d538fd9eaf54632a2880c175ed62ff24c0
NickBwalley <NickBwalley@gmail.com>
2022-09-16 22:25:18 +0300

    iterating through arrays components

--- src/index.js
+++ src/index.js
@@ -4,62 +4,43 @@ import ReactDOM from 'react-dom';
 //CSS
 import './index.css';
 
-const firstBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-  title: 'I love you to the moon and back',
-  author: 'Amelia Hepworth',
-};
+const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+    title: 'I love you to the moon and back',
+    author: 'Amelia Hepworth',
+  },
 
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51e1S2nD9IL._AC_UL320_.jpg',
-  title: 'the Road to React',
-  author: 'Robin Wieruch',
-};
+  {
+    img: 'https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51e1S2nD9IL._AC_UL320_.jpg',
+    title: 'the Road to React',
+    author: 'Robin Wieruch',
+  },
 
-const thirdBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/91HHxxtA1wL._AC_UL116_SR116,116_.jpg',
-  title: 'The wonderful Things You Will Be!',
-  author: 'Emily Winfield Martin',
-};
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/91HHxxtA1wL._AC_UL116_SR116,116_.jpg',
+    title: 'The wonderful Things You Will Be!',
+    author: 'Emily Winfield Martin',
+  },
+];
 
+const names = ['John', 'Solo', 'Nick'];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>;
+});
+console.log(newNames);
 const BookList = () => {
-  return (
-    <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-          doloremque, velit animi et aspernatur doloribus iusto dignissimos
-          molestiae? Quis, perferendis.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-
-      <Book
-        img={thirdBook.img}
-        title={thirdBook.title}
-        author={thirdBook.author}
-      />
-    </section>
-  );
+  return <section className="booklist">{newNames}</section>;
 };
 
 const Book = (props) => {
-  const { img, title, author, children } = props;
+  const { img, title, author } = props;
   console.log(props);
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
