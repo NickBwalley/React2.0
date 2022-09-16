cf3cef23589ce4a1e49dfd10e74c24e4dfc5fd12
NickBwalley <NickBwalley@gmail.com>
2022-09-16 22:47:54 +0300

    using the spread operator

--- src/index.js
+++ src/index.js
@@ -6,18 +6,21 @@ import './index.css';
 
 const books = [
   {
+    id: 1,
     img: 'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
     title: 'I love you to the moon and back',
     author: 'Amelia Hepworth',
   },
 
   {
+    id: 2,
     img: 'https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/71aLultW5EL._AC_UY218_.jpg',
     title: 'Our Class is a Family',
     author: 'Shannon Olsen',
   },
 
   {
+    id: 3,
     img: 'https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/71gGPRRlyTL._AC_UY218_.jpg',
     title: 'The Vanishing Half: A Novel',
     author: 'Emily Winfield MartinBrit Bennett',
@@ -28,14 +31,14 @@ const BookList = () => {
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book book={book}></Book>;
+        return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { img, title, author } = props.book;
+  const { img, title, author } = props;
 
   return (
     <article className="book">
