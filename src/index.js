e476f4addd03567b9b387fe40b00ff2da2e02bd6
NickBwalley <NickBwalley@gmail.com>
2022-09-16 21:57:38 +0300

    props destructuring

--- src/index.js
+++ src/index.js
@@ -47,12 +47,12 @@ const BookList = () => {
 };
 
 const Book = (props) => {
-  console.log(props);
+  const { img, title, author } = props;
   return (
     <article className="book">
-      <img src={props.img} alt="" />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
