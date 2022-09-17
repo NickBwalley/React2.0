d430ad191481c37f24f3cc4f3d39f8dd727d0e98
NickBwalley <NickBwalley@gmail.com>
2022-09-17 20:04:40 +0300

    event handling in react

--- src/index.js
+++ src/index.js
@@ -40,11 +40,35 @@ const BookList = () => {
 const Book = (props) => {
   const { img, title, author } = props;
 
+  const clickHandler = (e) => {
+    console.log(e.target);
+    alert('Hello World');
+  };
+
+  const complexExample = (author) => {
+    console.log(author);
+  };
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        Buy Me Coffee!
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          complexExample(author);
+        }}
+      >
+        Complex
+      </button>
     </article>
   );
 };
