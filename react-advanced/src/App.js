495c912634184b2c68c8c28349269b0e0d5e5502
NickBwalley <NickBwalley@gmail.com>
2022-09-19 07:41:35 +0300

    useState hook

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+
 function App() {
+  let title = 'Random Title';
+  const clickHandler = () => {
+    title = 'hello people!';
+    console.log(title);
+  };
   return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
-    </div>
-  )
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={clickHandler}>
+        Click me!
+      </button>
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
