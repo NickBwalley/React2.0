ba759526e82503029d1cf34ef2f1f686eb92d488
NickBwalley <NickBwalley@gmail.com>
2022-09-19 08:01:42 +0300

    useState Hook in REact

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
+import UseStateBasics from './tutorial/1-useState/setup/2-useState-basics';
 
 function App() {
-  let title = 'Random Title';
-  const clickHandler = () => {
-    title = 'hello people!';
-    console.log(title);
-  };
   return (
-    <React.Fragment>
-      <h2>{title}</h2>
-      <button type="button" className="btn" onClick={clickHandler}>
-        Click me!
-      </button>
-    </React.Fragment>
+    <div className="container">
+      <UseStateBasics />
+    </div>
   );
 }
 
--- react-advanced/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react-advanced/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,22 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState('Hello World!');
+  const handleClick = () => {
+    if (text === 'Hello World!') {
+      setText('React for life!!!');
+    } else {
+      setText('Hello World!');
+    }
+  };
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button type="buttton" className="btn" onClick={handleClick}>
+        Change title!
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
