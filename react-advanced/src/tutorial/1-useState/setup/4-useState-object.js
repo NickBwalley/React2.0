73ba4c2d2a27c763d46b1c1d89a96f4fa62065bd
NickBwalley <NickBwalley@gmail.com>
2022-09-20 23:29:17 +0300

    useState using Objects and SpreadOperator

--- react-advanced/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'Nick',
+    age: 24,
+    message: 'I love React coding',
+  });
+
+  const changeMessage = () => {
+    setPerson({ ...person, message: 'Coding for life' });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        Change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
