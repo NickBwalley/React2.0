f8af87aebda6225021c0c972bb46ee35382a5ed9
NickBwalley <NickBwalley@gmail.com>
2022-09-20 23:45:31 +0300

    useState simpleCounter

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import UseStateBasics from './tutorial/1-useState/setup/4-useState-object';
+import UseStateBasics from './tutorial/1-useState/setup/5-useState-counter';
 
 function App() {
   return (
