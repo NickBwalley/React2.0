50155b67765901694ddd05a2d0e8f30990489cc7
NickBwalley <NickBwalley@gmail.com>
2022-09-20 23:20:19 +0300

    useState Hook with an Array

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import UseStateBasics from './tutorial/1-useState/setup/2-useState-basics';
+import UseStateBasics from './tutorial/1-useState/setup/3-useState-array';
 
 function App() {
   return (
