24ff2b26145bda69a76c9d1075ce71d477e5dbe6
NickBwalley <NickBwalley@gmail.com>
2022-09-23 21:17:55 +0300

    useStateEffect Hook

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import UseStateBasics from './tutorial/1-useState/setup/3-useState-array';
+import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
 
 function App() {
   return (
     <div className="container">
-      <UseStateBasics />
+      <Setup />
     </div>
   );
 }
