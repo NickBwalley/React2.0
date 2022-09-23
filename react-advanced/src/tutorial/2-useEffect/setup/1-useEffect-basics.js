cf148e11000963d5f604b4409ea4a8935cde9f2a
NickBwalley <NickBwalley@gmail.com>
2022-09-23 21:31:08 +0300

    useEffect DependencyList

--- react-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -10,7 +10,11 @@ const UseEffectBasics = () => {
     if (value >= 1) {
       document.title = `New Messages(${value}) `;
     }
-  });
+  }, [value]);
+
+  useEffect(() => {
+    console.log('Hello World!');
+  }, []);
 
   console.log('render component');
 
