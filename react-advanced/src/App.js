73f338314fa61b718de974904a2f010df65a4742
NickBwalley <nickbwalley@gmail.com>
2022-10-06 20:58:31 +0300

    basic submit forms in react

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide';
+import React from 'react'
+import Setup from './tutorial/4-forms/setup/1-controlled-inputs'
 
 function App() {
   return (
-    <div className="container">
+    <div className='container'>
       <Setup />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
