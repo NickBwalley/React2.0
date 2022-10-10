4db89033b2c6aa89b64637a512863060f35c61a8
NickBwalley <nickbwalley@gmail.com>
2022-10-10 23:48:58 +0300

    multiple inputs reactJS

--- react-advanced/src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ react-advanced/src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -1,37 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
 // React
 // value, onChange
-// dynamic object keys
 
 const ControlledInputs = () => {
-  // const [firstName, setFirstName] = useState('')
-  // const [email, setEmail] = useState('')
-  // const [age, setAge] = useState('')
-  const [person, setPerson] = useState({ firstName: '', email: '', age: '' })
-  const [people, setPeople] = useState([])
-
+  const [person, setPerson] = useState({ firstName: '', email: '', age: '' });
+  const [people, setPeople] = useState([]);
   const handleChange = (e) => {
-    const name = e.target.name
-    const value = e.target.value
-    setPerson({ ...person, [name]: value })
-  }
-
+    const name = e.target.name;
+    const value = e.target.value;
+    setPerson({ ...person, [name]: value });
+  };
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (person.firstName && person.email && person.age) {
-      const newPerson = { ...person, id: new Date.getTime().toString() }
-      setPeople([...people, newPerson])
-      setPerson({ firstName: '', email: '', age: '' })
+      const newPerson = { ...person, id: new Date().getTime().toString() };
+      setPeople([...people, newPerson]);
+      setPerson({ firstName: '', email: '', age: '' });
     }
-  }
-
+  };
   return (
     <>
-      <article>
-        <form className='form'>
+      <article className='form'>
+        <form>
           <div className='form-control'>
             <label htmlFor='firstName'>Name : </label>
             <input
@@ -55,30 +48,32 @@ const ControlledInputs = () => {
           <div className='form-control'>
             <label htmlFor='age'>Age : </label>
             <input
-              type='text'
+              type='number'
               id='age'
               name='age'
               value={person.age}
               onChange={handleChange}
             />
           </div>
-          <button type='submit' onChange={handleSubmit}>
+          <button type='submit' className='btn' onClick={handleSubmit}>
             add person
           </button>
         </form>
-        {people.map((person, index) => {
-          const { id, firstName, email, age } = person
+      </article>
+      <article>
+        {people.map((person) => {
+          const { id, firstName, email, age } = person;
           return (
-            <div className='item' key={id}>
+            <div key={id} className='item'>
               <h4>{firstName}</h4>
-              <p>{age}</p>
               <p>{email}</p>
+              <p>{age}</p>
             </div>
-          )
+          );
         })}
       </article>
     </>
-  )
-}
+  );
+};
 
-export default ControlledInputs
+export default ControlledInputs;
